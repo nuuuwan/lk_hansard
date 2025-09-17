b0ffca14a6a91c5166f84d52f4f5064ee51b66f7
nuuuwan <nuuuwan@gmail.com>
2025-09-17 11:45:26 +0530

    [push_to_clients] Updated copy_from_pdf_scraper.sh

--- copy_from_pdf_scraper.sh
+++ copy_from_pdf_scraper.sh
@@ -1,7 +1,4 @@
 # HACK! Delete this once pdf_scrape is in pypi
-
-
-
 rm -rf src/pdf_scraper
 cp -r ../pdf_scraper/src/pdf_scraper src/
 git add src/pdf_scraper
@@ -12,6 +9,4 @@ cp -r ../pdf_scraper/src/utils_future src/
 git add src/utils_future
 git commit -m "[copy_from_pdf_scraper] Updated utils_future"
 
-cp ../pdf_scraper/requirements.txt requirements.txt
-# Don't commit
 
