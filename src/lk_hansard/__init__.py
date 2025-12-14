a91cf25d097e31fbbb620f9317178a3fd6953c7b
nuuuwan <nuuuwan@gmail.com>
2025-12-14 09:13:59 +0530

    feat: add imports for decade shard classes in __init__.py

--- src/lk_hansard/__init__.py
+++ src/lk_hansard/__init__.py
@@ -2,3 +2,6 @@
 # flake8: noqa: F408
 
 from lk_hansard.Hansard import Hansard
+from lk_hansard.Hansard2000s import Hansard2000s
+from lk_hansard.Hansard2010s import Hansard2010s
+from lk_hansard.Hansard2020s import Hansard2020s
